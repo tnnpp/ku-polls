f423aa64ea05c7cb374b8171162a11716bcf7674
Napasorn TEVARUT <napasorn.te@ku.th>
2023-09-07 22:30:05 +0700

    add end date and modified views.py

--- poll/models.py
+++ poll/models.py
@@ -1,9 +1,11 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django import forms
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField('end published date')
 
     def __str__(self):
         return self.question_text
@@ -11,6 +13,12 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return (now - datetime.timedelta(days=1)) <= self.pub_date <= now
+
+    def is_published(self):
+        return timezone.now() > self.pub_date
+
+    def can_vote(self):
+        return self.end_date > timezone.now() > self.pub_date
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- poll/views.py
+++ poll/views.py
@@ -14,7 +14,12 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        all_questions = Question.objects.all()
+        # check if the question is published recently and can vote
+        question = [q for q in all_questions if q.is_published() and q.can_vote() and q.was_published_recently()]
+        # sort and select last 5 question
+        question_index = sorted(question,key=lambda q: q.pub_date,reverse=True)[:5]
+        return question_index
 
 class DetailView(generic.DetailView):
     model = Question
@@ -24,7 +29,11 @@ class DetailView(generic.DetailView):
         """
         Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+        all_questions = Question.objects.all()
+        # check if the question is published recently and can vote
+        question = [q for q in all_questions if q.is_published()]
+
+        return Question.objects.filter(question_text__in=question)
 
 
 class ResultsView(generic.DetailView):
