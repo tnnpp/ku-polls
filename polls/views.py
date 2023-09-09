a76407bace648c38fc4ea94bf66932580f7740ac
Napasorn TEVARUT <napasorn.te@ku.th>
2023-09-09 21:47:39 +0700

    add go back to index

--- polls/views.py
+++ polls/views.py
@@ -40,6 +40,7 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     # check if can vote
