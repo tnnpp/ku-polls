fecc0c2d76ebc193abf4d93c988753dd0bbd1420
Napasorn TEVARUT <napasorn.te@ku.th>
2023-09-14 22:50:25 +0700

    update vote views for new model

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from .models import Question, Choice
+from .models import Question, Choice, Vote
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
@@ -60,25 +60,31 @@ def vote(request, question_id):
     user = request.user
     print("current user is", user.id, "login", user.username)
     print("Real name:", user.first_name, user.last_name)
-    # Check if the user can vote on this question.
+
     if not question.can_vote():
         # User cannot vote on this question, so display an error message.
-        messages.error(request, "The polls is not available")
+        messages.error(request, "The polls is not available.")
+        return render(request, 'polls/detail.html')
+
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form with an error message.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
+        messages.error(request, "You didn't select a choice.")
+        return render(request, "polls/detail.html", {'question': question})
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        # check if user already voted.
+        is_exist = Vote.objects.filter(choice__question=selected_choice.question, user=user).exists()
+        if is_exist:
+            messages.error(request, "You have already voted.")
+            return render(request, "polls/detail.html", {'question': question})
+        else:
+            # create new vote object.
+            vote = Vote(choice=selected_choice,user=user)
+            vote.save()
+            # Always return an HttpResponseRedirect after successfully dealing
+            # with POST data. This prevents data from being posted twice if a
+            # user hits the Back button.
+            return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
 
