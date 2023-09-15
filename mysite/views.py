9da09899b1d5f76a7317e5aa72cd66df62f02485
Napasorn TEVARUT <napasorn.te@ku.th>
2023-09-15 12:39:19 +0700

    Fix signup page

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,8 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 def signup(request):
@@ -8,16 +10,20 @@ def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             # get named fields from the form data
             username = form.cleaned_data.get('username')
-            # password input field is named 'password1'
+            # password input field is named 'password'
             raw_passwd = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_passwd)
             login(request, user)
-        return redirect('polls:index')
-        # what if form is not valid?
-        # we should display a message in signup.html
+            return redirect('polls:index')
+        else:
+            # display a message in signup.html
+            print(form.errors)
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{field}: {error}")
     else:
         # create a user form and display it the signup page
         form = UserCreationForm()
