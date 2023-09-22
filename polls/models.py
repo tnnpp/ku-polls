6a611057eea96aa3914f913f620c062cf965c767
Napasorn TEVARUT <napasorn.te@ku.th>
2023-09-22 21:12:19 +0700

    updating result page

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class Question(models.Model):
     """
     A class representing a question.
